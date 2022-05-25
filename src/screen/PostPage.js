1b265c364578d9aa875c163a98d4ac911674da31
markgmz92 <markgmz92@gmail.com>
2022-05-25 19:39:41 +0800

    [CM - 005] Comments component refactor

--- src/screen/PostPage.js
+++ src/screen/PostPage.js
@@ -2,11 +2,11 @@ import React, { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 
 import axios from 'axios';
-import Comments from '../components/Comments';
 
 const PostPage = () => {
   const { id } = useParams();
   const [post, setPost] = useState([]);
+  const [comments, setComments] = useState([]);
 
   useEffect(() => {
     const fetchPost = async () => {
@@ -18,6 +18,17 @@ const PostPage = () => {
     };
     fetchPost();
   }, [id]);
+
+  useEffect(() => {
+    const fetchComments = async () => {
+      const { data } = await axios.get(
+        'https://jsonplaceholder.typicode.com/comments?=postId'
+      );
+      console.log(comments);
+      setComments(data);
+    };
+    fetchComments();
+  }, []);
 
   return (
     <>
@@ -31,8 +42,13 @@ const PostPage = () => {
         <p>{post.body}</p>
         <div>
           <h3>This is the comments section</h3>
-
-          <Comments />
+          {comments.map((comment) => (
+            <div key={comment.post_id}>
+              <p>Name: {comment.name}</p>
+              <p>Email: {comment.email}</p>
+              <p>Comment: {comment.body}</p>
+            </div>
+          ))}
         </div>
       </div>
     </>
